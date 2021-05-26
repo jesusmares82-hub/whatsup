e795bacd452032793322425084b36ea2c89d4cfc
Jesus <jesusmares82@gmail.com>
2021-05-26 17:45:54 -0500

    lista documntacion con swagger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 const path = require("path");
 const express = require("express");
+const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
+const logger = require("morgan");
 const models = require("./models");
 const routes = require("./routes");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("../swagger.json");
+const fs = require("fs");
 require("dotenv").config();
 
-const app = express();
-
+//Middlewares
+app.use(cors()); //Implementará CORS en el servidor
+app.use(helmet()); // Implementará Helmet en el servidor
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(
+  logger("combined", {
+    stream: fs.createWriteStream("./access.log", { flags: "a" }),
+  })
+);
+app.use(
+  logger("combined", {
+    stream: fs.createWriteStream("./access.log", { flags: "a" }),
+  })
+);
 app.set("port", process.env.PORT || 8080);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -31,11 +50,14 @@ app.use((err, req, res, next) => {
       return res
         .status(403)
         .send({ message: "Ya existe un registro con el mismo valor" });
+
     default:
       console.log(err);
       return res
         .status(500)
-        .send("Ups tenemos un problema en el servidor, intentalo más tarde!");
+        .send(
+          `Ups tenemos un problema en el servidor, intentalo más tarde! ${err} `
+        );
   }
 });
 
